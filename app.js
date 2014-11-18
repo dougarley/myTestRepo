dc1b0455013da41249ca9692ad10c0dd3ef57d8c
Douglas Arley <darley@douglasarleymbp.fullsail.com>
2014-11-18 11:16:10 -0500

    Added my main label to my main window

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ var mainWindow = Ti.UI.createWindow({
   backgroundColor : '#fafafa'
 });
 
+var mainLabel = Ti.UI.createLabel({
+  text : 'This is my main label'
+});
+
+mainWindow.add(mainLabel);
 mainWindow.open();
