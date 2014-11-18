b539c20746a0c6446172d86a9ded20e15f53cc38
Douglas Arley <darley@douglasarleymbp.fullsail.com>
2014-11-18 11:16:51 -0500

    Added my main button

--- app.js
+++ app.js
@@ -6,5 +6,11 @@ var mainLabel = Ti.UI.createLabel({
   text : 'This is my main label'
 });
 
+
+var mainButton = Ti.UI.createButton({
+  title : 'Do something!'
+})
+
 mainWindow.add(mainLabel);
+mainWindow.add(mainButton);
 mainWindow.open();
